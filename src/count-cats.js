3d5899ad8de7a3dafa154960ffaedc4f4a49cd8e
Grigorovich Tatyana <gtm003@gmail.com>
2020-10-02 23:02:48 +0300

    feat: add count-cats.js & dream-team.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+    let result = 0;
+    function count (arr) {
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i] === '^^') result++;
+      }
+    }
+  backyard.forEach(count);
+  return result;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = false;
+  let nameTeam ='';
+    function FirstLetter (str) {
+      if (typeof str == 'string') nameTeam = nameTeam + str.trim()[0];
+    }
+  if (Array.isArray(members)) {
+    members.forEach(FirstLetter);
+    result = nameTeam.toUpperCase().split('').sort().join('');
+  } 
+  return result;
 };
