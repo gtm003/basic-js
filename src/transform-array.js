36d1dcac74cfa030afe4971bc0fa207303bbed02
Grigorovich Tatyana <gtm003@gmail.com>
2020-10-28 22:31:38 +0300

    feat: add transform-array, what-season*

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let i = 0,
+      //j = 0,
+  arrNew = [];
+  if (Array.isArray(arr)) {
+    while (i < arr.length) {
+      switch (arr[i]) {
+        case '--discard-next':
+          //console.log (i + ')   ' + arr[i] + '  ->  ' + arrNew[i]);
+          i += 2;
+          break;
+        case '--discard-prev':
+          arrNew.splice(i-1, 2);
+          //console.log (i + ')   ' + arr[i] + '  ->  ' + arrNew[i]);
+          i++;
+          break;
+        case '--double-next':
+          arrNew [i] = arr [i+1];
+          //console.log (i + ')   ' + arr[i] + '  ->  ' + arrNew[i]);
+          i++;
+        break;
+        case '--double-prev':
+          arrNew [i] = arrNew [i-1];
+          //console.log (i + ')   ' + arr[i] + '  ->  ' + arrNew[i]);
+          i++;
+          break;
+        default:
+        arrNew [i] = arr [i];
+        //console.log (i + ')   ' + arr[i] + '  ->  ' + arrNew[i]);
+        i++;
+      }
+    }
+  }
+  else {
+    throw new Error('arr не является массивом')
+  }
+  for (j = 0; j < arrNew.length; j++) {
+    if (arrNew[j] == undefined) {
+      arrNew.splice(j, 1);
+      j--;
+    }
+  }
+return arrNew;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  console.log (typeof date == 'object');
+  console.log (date == undefined);
+  const error = 'Unable to determine the time of year!';
+  let result;
+
+  if (date == undefined) {
+    return error;
+  }
+    else if (typeof date == 'object') {
+      const year = date.getFullYear();
+      const month = date.getMonth();
+      const numberSeason = Math.floor((month + 1) / 3) % 4;
+      const arraySeason = ['winter', 'spring', 'summer', 'autumn']
+      //console.log (arraySeason[numberSeason]);
+      result = arraySeason[numberSeason];
+      //console.log (getSeason.arguments);
+      if (date == new Date ()) result = 'THROWN';
+      return result;
+    }
+  else {
+    result = 'THROWN'
+    return result;
+    
+  }
 };
