cf12db392a87ba979241389cbc6c31037649bf42
Grigorovich Tatyana <gtm003@gmail.com>
2020-10-31 22:04:24 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let defaultOptions = { 
+  repeatTimes: 0,
+  separator: '+',
+  addition: '',
+  additionRepeatTimes: 0,
+  additionSeparator: '|' },
+  localOptions = Object.assign(defaultOptions, options);
+  /*
+  console.log(localOptions.repeatTimes);
+  console.log(localOptions.separator);
+  console.log(localOptions.addition);
+  console.log(localOptions.additionRepeatTimes);
+  console.log(localOptions.additionSeparator);
+  */
+  let resalt = '';
+  //resalt += str;
+  //resalt += localOptions.addition;
+  if (localOptions.repeatTimes > 0 || localOptions.repeatTimes == undefined) {
+    
+    for (i = 1; i < localOptions.repeatTimes; i++) {
+    resalt += str;
+    if (localOptions.additionRepeatTimes > 0) {
+      for (j = 1; j < localOptions.additionRepeatTimes; j++) {
+      resalt += localOptions.addition + localOptions.additionSeparator;
+    }
+    resalt += localOptions.addition;  'la+sla+sla+'
+    }
+    
+    resalt += localOptions.separator;
+    //resalt += str;
+    //resalt += localOptions.addition;
+  }
+  resalt += str;
+    if (localOptions.additionRepeatTimes > 0 || localOptions.additionRepeatTimes == undefined) {
+      for (j = 1; j < localOptions.additionRepeatTimes; j++) {
+      resalt += localOptions.addition + localOptions.additionSeparator;
+    }
+    resalt += localOptions.addition;
+  }
+  }
+  return resalt;
 };
   
